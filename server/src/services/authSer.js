10197571ab2a7049458c799c2343e4fe2ece323d
JuanMadrid99 <115056117+JuanMadrid99@users.noreply.github.com>
2025-05-03 02:06:14 -0600

    primero pasos con /services

---
+++ server/src/services/authSer.js
@@ -0,0 +1,26 @@
+/* SERVICIOS DE AUTENTICACIÓN DE USUARIOS*/
+import { comprobarUsuario } from '../models/authMod.js';
+
+export async function loginService(nickname, psw) {
+    const result = await comprobarUsuario(nickname, psw);
+    return result;
+}
+
+export function definirTipoUsuario(session) {
+    const user = {
+        username: session.user,
+        isAdmin: session.admin,
+        tipo: session.tipo,
+        id: 0
+    };
+
+    if (session.admin == undefined) return user;
+
+    if (session.admin === true) {
+        user.id = (session.tipo === 'Super Administrador') ? 1 : 2;
+    } else {
+        user.id = (session.tipo === 'Aplicativo') ? 3 : 4;
+    }
+
+    return user;
+}
