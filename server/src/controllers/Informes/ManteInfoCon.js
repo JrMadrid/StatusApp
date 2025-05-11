2b9f7280ca22ec676df644e20c6f86a0b5975b7e
JuanMadrid99 <115056117+JuanMadrid99@users.noreply.github.com>
2025-05-11 02:41:07 -0600

    services de mantenimientoInfo

--- server/src/controllers/Informes/ManteInfoCon.js
+++ server/src/controllers/Informes/ManteInfoCon.js
@@ -1,109 +1,86 @@
 /* CONTROLADORES DE INFORME -- MANTENIMIENTO */
-import sql from 'mssql';
+import { fechasMantenimientosRealizados, obtenerArchivoMantenimiento, obtenerArchivosMantenimientos } from '../../services/Informes/ManteInfoSer.js';
 
 // Manda las fechas vinculadas al economico
 const economico = async (req, res) => {
-    try {
-        if (req.session.admin != undefined) {
-            const numero = req.params.economico;
-            req.session.numeroMante = numero;
-            req.session.save(err => {
-                if (err) {
-                    console.error('Error al guardar el numero economico:', err);
-                }
-            });
-
-        } else {
-            res.redirect('');
-        }
-    } catch (error) {
-        console.error('Error :', error);
-    }
+	try {
+		if (req.session.admin != undefined) {
+			const numero = req.params.economico;
+			req.session.numeroMante = numero;
+			req.session.save(err => {
+				if (err) {
+					console.error('Error al guardar el numero economico:', err);
+				}
+			});
+		} else {
+			res.redirect('');
+		}
+	} catch (error) {
+		console.error('Error :', error);
+	}
 };
 
 // Manda las fechas vinculadas al economico
 const fechasr = async (req, res) => {
-    if (req.session.admin != undefined) {
-        try {
-            // await dbConnection(); solo se inicia la conexion al arrancar el servidor;
-            const economico = req.session.numeroMante;
+	if (req.session.admin != undefined) {
+		try {
+			const economico = req.session.numeroMante;
+			const fechasr = await fechasMantenimientosRealizados(economico)
 
-            const query = 'SELECT mante.fecharealizada AS realizado, mante.economico AS economico, sucu.nombre AS sucursal, sucu.ingresponsable as ingresponsable FROM mantenimiento mante INNER JOIN sucursales sucu ON mante.economico = sucu.economico WHERE mante.economico = @economico ORDER BY mante.fecharealizada DESC';
-            const request = new sql.Request();
+			return res.status(200).json(fechasr);
 
-            request.input('economico', sql.VarChar, economico);
-
-            const fechasr = await request.query(query);
-
-            return res.status(200).json(fechasr.recordset);
-
-        } catch (error) {
-            console.error('Error :', error);
-
-        } 
-    } else {
-        res.redirect('');
-    }
+		} catch (error) {
+			console.error('Error :', error);
+		}
+	} else {
+		res.redirect('');
+	}
 };
 
 // Manda el archivo de la constancia de la fecha seleccionada
 const info = async (req, res) => {
-    if (req.session.admin != undefined) {
-        try {
-            const fechasr = req.params.fechasr;
-            if (fechasr && fechasr !== null && fechasr !== 'null') {
-                // await dbConnection(); solo se inicia la conexion al arrancar el servidor;
-            
-                const economico = req.session.numeroMante;
+	if (req.session.admin != undefined) {
+		try {
+			const fechasr = req.params.fechasr;
 
-                const constanciaAr = await sql.query(`SELECT constancia FROM mantenimiento WHERE fecharealizada = '${fechasr}' AND economico = '${economico}'`);
+			if (fechasr && fechasr !== null && fechasr !== 'null') {
+				const economico = req.session.numeroMante;
+				const constanciaArchivo = await obtenerArchivoMantenimiento(fechasr, economico);
 
-                if (constanciaAr.recordset.length > 0) {
-                    const archivo = constanciaAr.recordset[0].constancia;
-                
-                    res.set('Content-Type', 'image/jpeg'); // Cambia el tipo de contenido a JPEG
-                    res.set('Content-Disposition', `inline; filename="constancia.jpg"`); // Cambia el nombre del archivo a descargar
-                
-                    res.status(200).send(archivo);
-                } else {
-                    res.status(404).json({ message: 'Archivo no encontrado' });
-                }
-            } else {
-                return res.status(400).json({ message: 'Fecha no valida' });
-            }
-        } catch (error) {
-            console.error('Error:', error);
-            res.status(500).send("Error al obtener los datos");
-        } 
-    } else {
-        res.redirect('');
-    }
+				res.set('Content-Type', 'image/jpeg'); // Cambia el tipo de contenido a JPEG
+				res.set('Content-Disposition', `inline; filename="constancia.jpg"`); // Cambia el nombre del archivo a descargar
+				res.status(200).send(constanciaArchivo);
+
+			} else {
+				return res.status(400).json({ message: 'Fecha no valida' });
+			}
+		} catch (error) {
+			console.error('Error:', error);
+			res.status(error.status || 500).json({ message: error.message || 'Error interno' }); // Responder con falla
+		}
+	} else {
+		res.redirect('');
+	}
 };
 
 // Manda todas las constancias
 const infos = async (req, res) => {
-    if (req.session.admin != undefined) {
-        try {
-            // await dbConnection(); solo se inicia la conexion al arrancar el servidor;
-            const economico = req.session.numeroMante
+	if (req.session.admin != undefined) {
+		try {
+			const economico = req.session.numeroMante;
+			const constancias = await obtenerArchivosMantenimientos(economico);
+			
+			res.status(200).json(constancias);
 
-            const constancias = await sql.query(`SELECT constancia FROM mantenimiento WHERE economico = '${economico}' AND constancia IS NOT NULL`);
-
-            if (constancias.recordset.length > 0) {            
-                const archivos = constancias.recordset.map(item => {
-                    return item.constancia;
-                });
-                res.status(200).json(archivos);
-            }
-        } catch (error) {
-            console.error('Error:', error);
-            res.status(500).send("Error al obtener los datos");
-        } 
-    } else {
-        res.redirect('');
-    }
+		} catch (error) {
+			console.error('Error:', error);
+			res.status(error.status || 500).send({ message: error.message || 'Error interno' });
+		}
+	} else {
+		res.redirect('');
+	}
 };
 
 export default {
-    economico, fechasr, info, infos
+	economico, fechasr, info, infos
 };
